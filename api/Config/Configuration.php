6e027633f74f633a989d8fa23346db34efe119aa
Tuan Anh <tuananh3668@gmail.com>
2015-05-28 22:34:58 +0800

    Change config

--- api/Config/Configuration.php
+++ api/Config/Configuration.php
@@ -1,7 +1,12 @@
 <?php
 	class Configuration {
-		const MYSQL_DATABASE = 'test';  
+		// const MYSQL_DATABASE = 'test';  
+		// const MYSQL_HOST = 'localhost';
+		// const MYSQL_USER = 'root';
+		// const MYSQL_PASSWORD = '';
+
+		const MYSQL_DATABASE = 'wovodat';
 		const MYSQL_HOST = 'localhost';
-		const MYSQL_USER = 'root';
-		const MYSQL_PASSWORD = '';
+		const MYSQL_USER = 'wovodatuser';
+		const MYSQL_PASSWORD = 'wovodatpassword';
 	}
