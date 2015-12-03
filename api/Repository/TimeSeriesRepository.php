b72839ac86abb1c377a2a778a6a829f65d2dd92e
FabioNgo <ngolebaotrung@gmail.com>
2015-12-03 17:49:01 +0800

    only get deformation data list for testing

--- api/Repository/TimeSeriesRepository.php
+++ api/Repository/TimeSeriesRepository.php
@@ -7,19 +7,10 @@ DEFINE('HOST', 'localhost');
 
 class TimeSeriesRepository {
 
-  private static function saveSerie($newSerie) {
-    $series = json_decode(file_get_contents('Serie/Series.json', true), true);
-    $found = false;
-    if($series != null){
-      foreach ($series as $key => $serie) {
-        if ($serie['sr_id'] == $newSerie['sr_id']) {
-          $series[$key] = $newSerie;
-          $found = true;
-        }
-      }
-    }
-    if (!$found)
-      $series[] = $newSerie;
+  private static function saveSerie($series) {
+     
+    
+    
     file_put_contents('Serie/Series.json', json_encode($series), FILE_USE_INCLUDE_PATH);
   }
 
@@ -36,12 +27,13 @@ class TimeSeriesRepository {
 	public static function getTimeSeriesList($vd_id) {
 		$result = array();
 		
-		$DATA_LIST = array( "Seismic", "Deformation", "Gas",  "Meteo" , "Hydrology");
+		// $DATA_LIST = array( "Seismic", "Deformation", "Gas",  "Meteo" , "Hydrology");
+    $DATA_LIST = array("Deformation");
 		foreach ($DATA_LIST as $value) {
 			$series = call_user_func_array($value."Repository::getTimeSeriesList", array($vd_id));
-			
+			self::saveSerie($series);
 			foreach ($series as $serie) {
-				self::saveSerie($serie);
+				
 				array_push($result, $serie);
 			}
 		}
@@ -55,11 +47,21 @@ class TimeSeriesRepository {
     if (!$serie)
       return null;
     // var_dump($serie);
-
+    $ids = array();
+    if(array_key_exists("ds_id", $serie)){
+      $ids['ds_id'] = $serie["ds_id"];
+    }
+    if(array_key_exists("ds_id1", $serie)){
+      $ids['ds_id1'] = $serie["ds_id1"];
+    }
+    if(array_key_exists("ds_id2", $serie)){
+      $ids['ds_id2'] = $serie["ds_id2"];
+    }
+    var_dump($ids);
     $serie['data'] = call_user_func_array( $serie['category']."Repository::getStationData" , 
       array( $serie['data_type'] , 
         isset( $serie['station_code']) ? $serie['station_code'] : $serie['volcanoID'],
-        $serie['component'], $serie["ds_id"] )  );
+        $serie['component'], $ids )  );
     // $serie['data'] = 
     return $serie;
   }
