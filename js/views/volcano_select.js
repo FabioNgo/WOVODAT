69e9cad9e8ea8c5ff39f15aa6324e49006988462
FabioNgo <ngolebaotrung@gmail.com>
2015-12-11 06:37:44 +0800

    can select volcano when add parameter vd_id in url: (/eruption/?volcano=vd_id)

--- js/views/volcano_select.js
+++ js/views/volcano_select.js
@@ -21,18 +21,44 @@ define(function(require) {
       this.collection = options.collection;
       this.collection.fetch();
       this.listenTo(this.collection, 'sync', this.render);
+      
     },
 
     render: function() {
       this.$el.html(this.template({
         volcanoes: this.collection.models
       }));
+      // read volcano parameter on url
+      var  vd_id = this.getUrlParameter("volcano");
+      for(var i=0;i<this.collection.models.length;i++){
+        if(vd_id == this.collection.models[i].id){
+          this.selectingVolcano.set('vd_id', vd_id); // .set auto call event in eventhandler 
+          this.selectingVolcano.trigger("update");
+          return;
+        }
+      }
+      
     },
 
     changeSelection: function(vd_id) {
+      
       this.$el.find('select').val(vd_id);
+      
     },
+    getUrlParameter: function(sParam) {
+      var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+          sURLVariables = sPageURL.split('&'),
+          sParameterName,
+          i;
 
+      for (i = 0; i < sURLVariables.length; i++) {
+          sParameterName = sURLVariables[i].split('=');
+
+          if (sParameterName[0] === sParam) {
+              return sParameterName[1] === undefined ? true : sParameterName[1];
+          }
+      }
+    },
     onSelectChange: function() {
       var vd_id = this.$el.find('select').val();
       if (vd_id) {
